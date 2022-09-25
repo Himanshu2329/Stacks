4dfa01e92fc99dac812da74f99b9142fc60fd806
Himanshu2329 <kumarhimanshu4390@gmail.com>
2022-09-25 18:15:40 +0530

    Basic Done

---
+++ Intro.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+/**
+ * Intro
+ */
+public class Intro {
+
+    public static void main(String[] args) {
+        Stack<Integer> st=new Stack<>();
+        st.push(12);
+        st.push(121);
+        st.push(120);
+        System.out.println(st);
+        st.push(12000);
+        st.push(12000);
+        System.out.println(st);
+        System.out.println(st.peek()+" "+ st.size());
+        st.pop();
+        System.out.println(st);
+    }
+}
